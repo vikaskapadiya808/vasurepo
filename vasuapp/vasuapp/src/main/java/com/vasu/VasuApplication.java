5052dc729fb447afdf512bf87b0cf5a55f9d787e
vikas kapadiya <vikaskapadiya808@gmail.com>
2025-10-07 09:11:58 +0000

    db details changed and packaging type in pom

--- vasuapp/vasuapp/src/main/java/com/vasu/VasuApplication.java
+++ vasuapp/vasuapp/src/main/java/com/vasu/VasuApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VasuApplication extends SpringBootServletInitializer {
+public class VasuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VasuApplication.class, args);
